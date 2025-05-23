8f45815a56e27bed68f18533cd6d34cf633a8260
ll.zhao <ll.zhao@foundtec.com>
2025-05-23 16:24:47 +0800

    tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        'times-1': '2px',
+        'times-2': '4px',
+        'times-3': '6px',
+        'times-4': '8px'
+      }
+    }
+  },
+  plugins: []
+}
