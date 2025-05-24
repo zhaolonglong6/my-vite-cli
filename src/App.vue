57713cb336d934a701ac20387d869cbd2e8ca470
ll.zhao <ll.zhao@foundtec.com>
2025-05-24 18:01:13 +0800

    框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="h-screen p-times-4 w-screen bg-primary-normal text-secondary text-f100">111</div>
+  <div class="h-screen p-times-4 w-screen bg-primary-normal text-white text-[20px]">
+    <Name />
+    <Age />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Name from './pages/file/index.vue'
+import Age from './pages/rab/index.vue'
+</script>
---
+++ src/pages/rab/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ age }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { User } from '.'
+
+const age = ref<User>({
+  id: 1,
+  age: 19
+})
+</script>
